4319375d5a1c7d6ce1837360490103f7bb6c3305
Raul Sanchez <raulsg93@gmail.com>
2020-12-15 15:40:20 -0600

    Raul Solution 111620

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,11 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show 
+        # finding dog using information from our URL 
+        @dog = Dog.find(params[:id])
+    end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,43 @@
 class EmployeesController < ApplicationController
+
+    before_action :current_employee, only: [:show, :edit, :update, :destroy]
+    def index
+        @employees = Employee.all
+    end
+
+    def show 
+    end
+
+    def new 
+        @employee = Employee.new
+    end
+
+    def create 
+        employee = Employee.create(employee_params)
+        redirect_to employee_path(employee) #'/employees/#{employee.id}'
+    end
+
+    def edit 
+    end
+
+    def update 
+        # find employee to be updated
+        @employee = Employee.find(params[:id]) 
+        @employee.update(employee_params)
+        redirect_to @employee
+     
+    end
+
+    def destroy
+        @employee.destroy
+        redirect_to employees_path
+    end
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :office, :title, :dog_id)
+    end
+
+    def current_employee
+        @employee = Employee.find(params[:id])
+    end
 end
